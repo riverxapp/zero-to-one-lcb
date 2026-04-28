8d138c9caa4c546081d5d1a39fdf8bdfde26cfdd
Chirag Dodiya <hi@chirag.co>
2026-04-28 18:19:43 +0530

    chore: deterministic patch step 1/1 - src/app/routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "../components/layout/AppLayout";
 import { Home } from "../pages/home";
 import { NotFoundPage } from "../pages/NotFoundPage";
+import { ProjectsIntakePage } from "../pages/projects";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<ProjectsIntakePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
